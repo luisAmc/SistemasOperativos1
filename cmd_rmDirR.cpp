2f27842a85519be76d54a48ca8a5ec4583ff300e
Luis Martinez <luis_amc@unitec.edu>
2015-09-13 17:35:40 -0400

    Con el rm -R, no autocompleta

--- cmd_rmDirR.cpp
+++ cmd_rmDirR.cpp
@@ -5,26 +5,34 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-	DIR *dir;
+	DIR *dir, *dir2;
 	struct dirent *entry;
+	int folder_count = 1;
 
 	stack<string> folder_stack;
 	string actual_folder = "./" + (string)argv[0];
 	folder_stack.push(actual_folder);
+	
 
 	//cout << "El folder actual es " << actual_folder << endl;
 
-	if (dir = opendir((char *)folder_stack.top().c_str())) 
-		while (entry = readdir(dir)) 
-			if (entry->d_type == DT_DIR) 
-				if ((((string)entry->d_name).compare(".") != 0) && (((string)entry->d_name).compare("..") != 0)) {
-					actual_folder += "/" + (string)entry->d_name; 
-					folder_stack.push(actual_folder);
-				}
-
+	while (folder_count > 0) {
+		folder_count = 0;
+		if (dir = opendir((char *)folder_stack.top().c_str())) {
+			actual_folder = folder_stack.top();
+			while (entry = readdir(dir)) 
+				if (entry->d_type == DT_DIR) {
+					folder_count++;
+					if ((((string)entry->d_name).compare(".") != 0) && (((string)entry->d_name).compare("..") != 0))
+						folder_stack.push(actual_folder + "/" + (string)entry->d_name);
+				} else
+					unlink((char *)(actual_folder + "/" + entry->d_name).c_str());
+		}
+	}
+	closedir(dir);
 
 	while (!folder_stack.empty()) {
-		cout << "Thread dice: " << folder_stack.top() << endl;
+		rmdir((char *)(folder_stack.top().c_str()));
 		folder_stack.pop();
 	}
 	
